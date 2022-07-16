e1baae2c57121b312b27afa2e200661c947f24ca
Team Alisha <109093182+TeamAlisha@users.noreply.github.com>
2022-07-16 13:56:58 +0530

    Add files via upload

---
+++ AlishaMusic/__init__.py
@@ -0,0 +1,40 @@
+
+
+from AlishaMusic.core.bot import AlishaBot
+from AlishaMusic.core.dir import dirr
+from AlishaMusic.core.git import git
+from AlishaMusic.core.userbot import Userbot
+from AlishaMusic.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = AlishaBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
