0cc406d39e44ae3252d697fb60f5aa42e2434851
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-17 13:59:33 +0530

    Update toptracks.py
    Signed-off-by: ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>

--- Abhimnayu/plugins/Robot/toptracks.py
+++ Abhimnayu/plugins/Robot/toptracks.py
@@ -4,14 +4,14 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup
 
 from config import BANNED_USERS
-from AnonX import app
-from AnonX.utils.database import (get_global_tops,
+from Abhimnayu import app
+from Abhimnayu.utils.database import (get_global_tops,
                                        get_particulars, get_userss)
-from AnonX.utils.decorators.language import languageCB
-from AnonX.utils.inline.playlist import (botplaylist_markup,
+from Abhimnayu.utils.decorators.language import languageCB
+from Abhimnayu.utils.inline.playlist import (botplaylist_markup,
                                               failed_top_markup,
                                               top_play_markup)
-from AnonX.utils.stream.stream import stream
+from Abhimnayu.utils.stream.stream import stream
 
 loop = asyncio.get_running_loop()
 
