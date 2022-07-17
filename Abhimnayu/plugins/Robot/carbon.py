d97aef806e479e0c9a4a4c64ec4703e38c472d25
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-17 09:32:01 +0530

    Update carbon.py
    Signed-off-by: ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>

--- Abhimnayu/plugins/Robot/carbon.py
+++ Abhimnayu/plugins/Robot/carbon.py
@@ -1,7 +1,7 @@
 from pyrogram.types import Message
 from pyrogram import filters
-from AnonX import app, aiohttpsession as aiosession
-from AnonX.utils.errors import capture_err
+from Abhimnayu import app, aiohttpsession as aiosession
+from Abhimnayu.utils.errors import capture_err
 from asyncio import gather
 from io import BytesIO
 
