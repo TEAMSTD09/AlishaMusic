2a029fcd3890d62e7d07ea9f583d32b2314527df
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-17 09:39:32 +0530

    Update live.py
    Signed-off-by: ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>

--- Abhimnayu/plugins/Robot/live.py
+++ Abhimnayu/plugins/Robot/live.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 
 from config import BANNED_USERS
-from AnonX import YouTube, app
-from AnonX.utils.channelplay import get_channeplayCB
-from AnonX.utils.decorators.language import languageCB
-from AnonX.utils.stream.stream import stream
+from Abhimnayu import YouTube, app
+from Abhimnayu.utils.channelplay import get_channeplayCB
+from Abhimnayu.utils.decorators.language import languageCB
+from Abhimnayu.utils.stream.stream import stream
 
 
 @app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
