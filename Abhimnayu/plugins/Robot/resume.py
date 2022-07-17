b1650bfe65d10ba6f1cc2296c8d0c69b736e7582
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-17 13:49:23 +0530

    Update resume.py
    Signed-off-by: ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>

--- Abhimnayu/plugins/Robot/resume.py
+++ Abhimnayu/plugins/Robot/resume.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AnonX import app
-from AnonX.core.call import Anon
-from AnonX.utils.database import is_music_playing, music_on
-from AnonX.utils.decorators import AdminRightsCheck
+from Abhimnayu import app
+from Abhimnayu.core.call import Anon
+from Abhimnayu.utils.database import is_music_playing, music_on
+from Abhimnayu.utils.decorators import AdminRightsCheck
 
 # Commands
 RESUME_COMMAND = get_command("RESUME_COMMAND")
