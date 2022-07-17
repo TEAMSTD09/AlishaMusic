4ea91d487a0528f771743ebd583bbbd10f30d697
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-17 09:29:02 +0530

    Update autoleave.py
    Signed-off-by: ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>

--- Abhimnayu/plugins/Robot/autoleave.py
+++ Abhimnayu/plugins/Robot/autoleave.py
@@ -2,9 +2,9 @@ import asyncio
 from datetime import datetime
 
 import config
-from AnonX import app
-from AnonX.core.call import Anon, autoend
-from AnonX.utils.database import (get_client, is_active_chat,
+from Abhimnayu import app
+from Abhimnayu.core.call import Anon, autoend
+from Abhimnayu.utils.database import (get_client, is_active_chat,
                                        is_autoend)
 
 
