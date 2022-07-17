128f59d5debcaf97098bce68c4e875a22e773598
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-17 13:50:56 +0530

    Update seeker.py
    Signed-off-by: ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>

--- Abhimnayu/plugins/Robot/seeker.py
+++ Abhimnayu/plugins/Robot/seeker.py
@@ -3,14 +3,14 @@ import asyncio
 from pyrogram.types import InlineKeyboardMarkup
 
 from strings import get_string
-from AnonX.misc import db
-from AnonX.utils.database import (get_active_chats, get_lang,
+from Abhimnayu.misc import db
+from Abhimnayu.utils.database import (get_active_chats, get_lang,
                                        is_music_playing)
-from AnonX.utils.formatters import seconds_to_min
-from AnonX.utils.inline import (stream_markup_timer,
+from Abhimnayu.utils.formatters import seconds_to_min
+from Abhimnayu.utils.inline import (stream_markup_timer,
                                      telegram_markup_timer)
 
-from AnonX.plugins.Robot.callback import wrong
+from Abhimnayu.plugins.Robot.callback import wrong
 
 checker = {}
 
