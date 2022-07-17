4607ba316a7db94a47737d721a1ef443bca868d7
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-17 09:22:13 +0530

    Update __init__.py
    Signed-off-by: ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>

--- Abhimnayu/__init__.py
+++ Abhimnayu/__init__.py
@@ -1,8 +1,8 @@
-from AnonX.core.bot import AnonXBot
-from AnonX.core.dir import dirr
-from AnonX.core.git import git
-from AnonX.core.userbot import Userbot
-from AnonX.misc import dbb, heroku, sudo
+from Abhimnayu.core.bot import AnonXBot
+from Abhimnayu.core.dir import dirr
+from Abhimnayu.core.git import git
+from Abhimnayu.core.userbot import Userbot
+from Abhimnayu.misc import dbb, heroku, sudo
 from aiohttp import ClientSession
 
 from .logging import LOGGER
