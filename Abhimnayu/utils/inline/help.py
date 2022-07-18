e6d0dc2b7336f73bea5c760c2ffcc909b75bdf81
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-18 06:10:43 +0530

    Update help.py
    Signed-off-by: ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>

--- Abhimnayu/utils/inline/help.py
+++ Abhimnayu/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from Abhimanyu import app
+from Abhimnayu import app
 from config import SUPPORT_GROUP
 
 
