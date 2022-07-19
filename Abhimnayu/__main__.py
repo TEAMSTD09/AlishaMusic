d683218fa1764b5d40daece658b8b456df55f667
Team Alisha <109093182+TeamAlisha@users.noreply.github.com>
2022-07-19 06:40:04 +0530

    Update __main__.py
    Signed-off-by: Team Alisha  <109093182+TeamAlisha@users.noreply.github.com>

--- Abhimnayu/__main__.py
+++ Abhimnayu/__main__.py
@@ -23,7 +23,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("AnonX").error(
+        LOGGER("Abhimnayu").error(
             "WTF Baby ! Atleast add a pyrogram string, How Cheap..."
         )
         return
@@ -31,7 +31,7 @@ async def init():
         not config.SPOTIFY_CLIENT_ID
         and not config.SPOTIFY_CLIENT_SECRET
     ):
-        LOGGER("AnonX").warning(
+        LOGGER("Abhimnayu").warning(
             "Spotify Client Id & Secret not added, Chutiya Saala ek itni simple cheej nahi laa paaya."
         )
     try:
@@ -45,8 +45,8 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("AnonX.plugins" + all_module)
-    LOGGER("AnonX.plugins").info(
+        importlib.import_module("Abhimnayu.plugins" + all_module)
+    LOGGER("Abhimnayu.plugins").info(
         "Necessary Modules Imported Successfully."
     )
     await userbot.start()
@@ -56,17 +56,17 @@ async def init():
             "https://telegra.ph/file/8d5db123638c2f6bb6ce4.mp4"
         )
     except NoActiveGroupCall:
-        LOGGER("AnonX").error(
+        LOGGER("Abhimnayu").error(
             "[ERROR] - \n\nHey Baby, firstly open telegram and turn on voice chat in Logger Group else fu*k off. If you ever ended voice chat in log group i will stop working and users will fu*k you up."
         )
         sys.exit()
     except:
         pass
     await Anon.decorators()
-    LOGGER("AnonX").info("Music Bot Started Successfully, Now Gib your girlfriend to @VeNom_Hai_HuM")
+    LOGGER("Abhimnayu").info("Music Bot Started Successfully, Now Gib your girlfriend to @VeNom_Hai_HuM")
     await idle()
 
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("AnonX").info("Stopping Music Bot, Bhakk Bhosdike (Gaand Maraa Tu)")
+    LOGGER("Abhimanyu").info("Stopping Music Bot, Bhakk Bhosdike (Gaand Maraa Tu)")
